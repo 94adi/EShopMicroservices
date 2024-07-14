9696d5e91607e4aa511f56f127174b820eb2c777
94adi <adrianrcotuna@gmail.com>
2024-07-14 14:27:41 +0300

    Catalog microservice 1 | Added Product Handler/Endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    //request
+    public record CreateProductCommand(string Name, 
+        List<string> Category, 
+        string Description, 
+        string ImageFile, 
+        decimal Price) : ICommand<CreateProductResult>;
+
+    //response
+    public class CreateProductResult
+    {
+        public Guid Id { get; set; }
+    }
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            //TODO: save to DB
+            Guid g = Guid.NewGuid();
+            return new CreateProductResult { Id = g };
+
+        }
+    }
+}
