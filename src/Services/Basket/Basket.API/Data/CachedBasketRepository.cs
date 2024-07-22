f6671346bae23c6a65ccb99ec51799208b867455
94adi <adrianrcotuna@gmail.com>
2024-07-22 18:46:10 +0300

    Basket microservice | 2 Redis + full env containerization

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IShoppingCartRepository repository,
+        IDistributedCache cache) : IShoppingCartRepository
+    {
+        public async Task<bool> DeleteShoppingCartAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            await cache.RemoveAsync(userName, cancellationToken);
+
+            return await repository.DeleteShoppingCartAsync(userName, cancellationToken);
+        }
+
+        public async Task<ShoppingCart> GetShoppingCartAsync(string userName, CancellationToken cancellationToken = default)
+        {
+            var cachedShoppingCart = await cache.GetStringAsync(userName, cancellationToken);
+            ShoppingCart shoppingCart = null;
+
+            if (!string.IsNullOrEmpty(cachedShoppingCart))
+            {
+                shoppingCart = JsonSerializer.Deserialize<ShoppingCart>(cachedShoppingCart);
+            }
+
+            if(shoppingCart is not null)
+            {
+                return shoppingCart;
+            }
+
+            shoppingCart = await repository.GetShoppingCartAsync(userName, cancellationToken);
+
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(shoppingCart), cancellationToken);
+
+            return shoppingCart;
+        }
+
+        public async Task<ShoppingCart> StoreShoppingCartAsync(ShoppingCart shoppingCart, CancellationToken cancellationToken = default)
+        {
+            var shoppingCartResult = await repository.StoreShoppingCartAsync(shoppingCart, cancellationToken);
+
+            await cache.SetStringAsync(shoppingCart.UserName, JsonSerializer.Serialize(shoppingCart), cancellationToken);
+
+            return shoppingCartResult;
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,8 +1,14 @@
 using BuildingBlocks.Exceptions.Handler;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
+
+var databaseConnectionString = builder.Configuration.GetConnectionString("Database");
+
+var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
 
 builder.Services.AddCarter();
 
@@ -15,18 +21,34 @@ builder.Services.AddMediatR(config =>
 
 builder.Services.AddMarten(opts =>
 {
-    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+    opts.Connection(databaseConnectionString!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.Decorate<IShoppingCartRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration = redisConnectionString;
+});
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(databaseConnectionString!)
+    .AddRedis(redisConnectionString!);
 
 var app = builder.Build();
 
 app.MapCarter();
 
 app.UseExceptionHandler(opt => { });
+
+app.UseHealthChecks("/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 
 app.Run();
